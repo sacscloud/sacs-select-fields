853093bc3a1d93166ef3ff902d47066a6a254ff5
yhoshua8a <yhoshua8a@gmail.com>
2019-04-03 15:51:07 -0600

    feat: initial structure

---
+++ sacs-select-fields.js
@@ -0,0 +1,14 @@
+    /**
+     * `sacs-select-fields`
+     * select field to show in data table
+     *
+     * @customElement
+     * @polymer
+     * @demo demo/index.html
+     */
+ 
+     'use strict';
+
+     Polymer({
+         is:'sacs-select-fields'
+     });
